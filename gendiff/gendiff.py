d90347c3064f39dd22f01566f7d15d1993e336fa
Mqtaw <mqtaw@yandex.ru>
2021-04-11 20:20:03 +0300

    Try to make maintability better

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -17,17 +17,20 @@ def generate_diff(path_to_file1, path_to_file2):
     keys = list(file1.keys() | file2.keys())
     keys.sort()
     for key in keys:
-        if key in file1 and key not in file2:
-            differences += '- {}: {}'.format(key, file1[key])
-        elif key in file1 and key in file2 and file1[key] == file2[key]:
-            differences += '  {}: {}'.format(key, file1[key])
-        elif key in file1 and key in file2 and file1[key] != file2[key]:
-            differences += '- {}: {}\n'.format(key, file1[key])
-            differences += '+ {}: {}'.format(key, file2[key])
-        elif key not in file1 and key in file2:
-            differences += '+ {}: {}'.format(key, file2[key])
-
+        differences += add_string_to_differences(file1, file2, key)
         if key != keys[-1]:
             differences += '\n'
-
     return differences
+
+
+def add_string_to_differences(file1, file2, key):
+    if key in file1 and key not in file2:
+        return '- {}: {}'.format(key, file1[key])
+    elif key in file1 and key in file2 and file1[key] == file2[key]:
+        return '  {}: {}'.format(key, file1[key])
+    elif key in file1 and key in file2 and file1[key] != file2[key]:
+        temp = '- {}: {}\n'.format(key, file1[key])
+        temp += '+ {}: {}'.format(key, file2[key])
+        return temp
+    elif key not in file1 and key in file2:
+        return '+ {}: {}'.format(key, file2[key])
